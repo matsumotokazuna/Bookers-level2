5b35caea395024bd27ac1aeb18d9b93efa19079f
vagrant <vagrant@localhost.localdomain>
2020-03-25 15:16:54 +0000

    [add]book-registration

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root :to => 'homes#index'
   get '/homes/about' => 'homes#about'
-  resources :books, only: [:create]
   resources :users, only: [:show, :edit, :update]
+  resources :books, only: [:create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
