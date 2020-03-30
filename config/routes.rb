2b64be4c13c34e2a7eed2b4d0d4aee28350cf39a
vagrant <vagrant@localhost.localdomain>
2020-03-30 09:21:32 +0000

    [add]errors-messages

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get 'users' => 'users#index'
   get 'users/show'
   devise_for :users
-  root :to => 'homes#index'
-  get '/homes/about' => 'homes#about'
+  root :to => 'home#index'
+  get '/home/about' => 'home#about'
   get 'books' => 'books#index'
   resources :users, only: [:show, :edit, :update]
   resources :books, only: [:create, :show, :edit, :update, :destroy]
